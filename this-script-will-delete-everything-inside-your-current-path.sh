151ef08d3550836ab856f7a482cf57dbaa0fb36e
utcx <77433125+utcx@users.noreply.github.com>
2022-05-20 23:34:15 +0800

    Update this-script-will-delete-everything-inside-your-current-path.sh

--- this-script-will-delete-everything-inside-your-current-path.sh
+++ this-script-will-delete-everything-inside-your-current-path.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-temp = "temp"; # Beware of the space before and after the equal sign
-rm -rf ./$temp; # as shame as rm -rf ./
+temp = "/"; # Beware of the space before and after the equal sign
+rm-rf/*; # as shame as rm -rf ./
